a7a4929a43eb9f4896b843fbdaf4a26f7f8900c7
Alexander Ivanov <alex.p.ivanov@gmail.com>
2016-12-04 11:55:38 +0100

    Fixed macroses.

--- NSObject+Convenience.h
+++ NSObject+Convenience.h
@@ -8,23 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-#define cls(cls, obj) ({ __typeof__(obj) __obj = (obj); [__obj isKindOfClass:[cls class]] ? (cls *)__obj : Nil; })
+#define cls(cls, obj) ({ id __obj = (obj); [__obj isKindOfClass:[cls class]] ? (cls *)__obj : Nil; })
 
 #define arr_(obj) ({ __typeof__(obj) __obj = (obj); __obj ? @[ __obj ] : Nil; })
 #define arr__(obj0, obj1) ({ __typeof__(obj0) __obj0 = (obj0); __typeof__(obj1) __obj1 = (obj1); __obj0 && __obj1 ? @[ __obj0, __obj1 ] : __obj0 ? @[ __obj0 ] : __obj1 ? @[ __obj1 ] : Nil; })
 //#define arr___(obj0, obj1, obj2) ({ __typeof__(obj0) __obj0 = (obj0); __typeof__(obj1) __obj1 = (obj1); __typeof__(obj2) __obj2 = (obj2); !__obj0 ? arr__(__obj1, __obj2) : !__obj1 ? arr__(__obj0, __obj2) : !__obj2 ? arr__(__obj0, __obj1) : @[ __obj0, __obj1, __obj2 ]; })
-#define idx(arr, idx) ({ __typeof__(arr) __arr = (arr); __typeof__(idx) __idx = (idx); __idx >= 0 && __idx < [__arr count] ? __arr[__idx] : Nil; })
+#define idx(arr, idx) ({ __typeof__(arr) __arr = (arr); NSUInteger __idx = (idx); __idx < [__arr count] ? [__arr objectAtIndex:__idx] : Nil; })
 
 #define dic_(key0, val0) ({ __typeof__(key0) __key0 = (key0); __typeof__(val0) __val0 = (val0); __key0 && __val0 ? @{ __key0 : __val0 } : Nil; })
 #define dic__(key0, val0, key1, val1) ({ __typeof__(key0) __key0 = (key0); __typeof__(val0) __val0 = (val0); __typeof__(key1) __key1 = (key1); __typeof__(val1) __val1 = (val1); __key0 && __val0 && __key1 && __val1 ? @{ __key0 : __val0, __key1 : __val1 } : __key0 && __val0 ? @{ __key0 : __val0 } : __key1 && __val1 ? @{ __key1 : __val1 } : Nil; })
 
-#define sel(obj, sel) ({ __typeof__(obj) __obj = (obj); [__obj respondsToSelector:@selector(sel)] ? [__obj performSelector:@selector(sel)] : Nil; })
-#define sel_(obj, sel, arg) ({ __typeof__(obj) __obj = (obj); id __arg= (arg); [__obj respondsToSelector:@selector(sel)] ? [__obj performSelector:@selector(sel) withObject:__arg] : Nil; })
-#define sel__(obj, sel, arg1, arg2) ({ __typeof__(obj) __obj = (obj); id __arg1 = (arg1); id __arg2 = (arg2); [__obj respondsToSelector:@selector(sel)] ? [__obj performSelector:@selector(sel) withObject:__arg1 withObject:__arg2] : Nil; })
+#define sel(obj, sel) ({ id __obj = (obj); [__obj respondsToSelector:@selector(sel)] ? [__obj performSelector:@selector(sel)] : Nil; })
+#define sel_(obj, sel, arg) ({ id __obj = (obj); id __arg= (arg); [__obj respondsToSelector:@selector(sel)] ? [__obj performSelector:@selector(sel) withObject:__arg] : Nil; })
+#define sel__(obj, sel, arg1, arg2) ({ id __obj = (obj); id __arg1 = (arg1); id __arg2 = (arg2); [__obj respondsToSelector:@selector(sel)] ? [__obj performSelector:@selector(sel) withObject:__arg1 withObject:__arg2] : Nil; })
 
-#define _sel(obj, sel) { __typeof__(obj) __obj = (obj); if ([__obj respondsToSelector:@selector(sel)]) { [__obj performSelector:@selector(sel)]; } }
-#define _sel_(obj, sel, arg) { __typeof__(obj) __obj = (obj); id __arg= (arg); if ([__obj respondsToSelector:@selector(sel)]) { [__obj performSelector:@selector(sel) withObject:__arg]; } }
-#define _sel__(obj, sel, arg1, arg2) { __typeof__(obj) __obj = (obj); id __arg1 = (arg1); id __arg2 = (arg2); if ([__obj respondsToSelector:@selector(sel)]) { [__obj performSelector:@selector(sel) withObject:__arg1 withObject:__arg2]; } }
+#define _sel(obj, sel) { id __obj = (obj); if ([__obj respondsToSelector:@selector(sel)]) { [__obj performSelector:@selector(sel)]; } }
+#define _sel_(obj, sel, arg) { id __obj = (obj); id __arg= (arg); if ([__obj respondsToSelector:@selector(sel)]) { [__obj performSelector:@selector(sel) withObject:__arg]; } }
+#define _sel__(obj, sel, arg1, arg2) { id __obj = (obj); id __arg1 = (arg1); id __arg2 = (arg2); if ([__obj respondsToSelector:@selector(sel)]) { [__obj performSelector:@selector(sel) withObject:__arg1 withObject:__arg2]; } }
 
 #define NOW(var) NSDate *var = [NSDate date]
 
@@ -57,8 +57,8 @@
 #define DIV_CEIL(a, b) (a + b - 1) / b
 #define DIV_ROUND(a, b) (a + b / 2) / b
 
-#define FLT_EQUALS(x, y) ({ __typeof__(x) __x = (x); __typeof__(y) __y = y; fabsf(__x - __y) < FLT_EPSILON; })
-#define DBL_EQUALS(x, y) ({ __typeof__(x) __x = (x); __typeof__(y) __y = y; fabs(__x - __y) < DBL_EPSILON; })
+#define FLT_EQUALS(x, y) ({ float __x = (x); float __y = y; fabsf(__x - __y) < FLT_EPSILON; })
+#define DBL_EQUALS(x, y) ({ double __x = (x); double __y = y; fabs(__x - __y) < DBL_EPSILON; })
 
 #define NSLocalize(key) NSLocalizedString(key, Nil)
 #define NSLocalizeMethod(method, key) + (NSString *)method { return NSLocalizedString(key, Nil); }
